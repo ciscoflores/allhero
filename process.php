741266d8df9c38073fdb1ad3ea9524fb25d207c7
Francisco Flores <francisco.flores@crowdint.com>
2014-11-30 08:15:18 -0600

    Update Simple Form

--- process.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Get Data
-$email = strip_tags($_POST['email']);
-
-// Send Message
-mail( "allheroapp@gmail.com", "Forma de suscripción",
-"Email: $email\n",
-"From: Forms <forms@example.net>" );
-?>
